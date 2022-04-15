6bb5cbb1413720a8db1c67aa21db5d00d92018f2
Duy Nguyen <duynguyen@Duys-MacBook-Pro-2.local>
2022-04-15 13:26:50 -0700

    done1

--- final-project-the-goats-main/client/src/visualizers/anhduynguyen8598.tsx
+++ final-project-the-goats-main/client/src/visualizers/anhduynguyen8598.tsx
@@ -6,11 +6,10 @@ import * as Tone from 'tone';
 import { Visualizer } from '../Visualizers';
 
 export const CircularVisualizer = new Visualizer(
-  'Flower_Duy-Nguyen',
+  'anhduynguyen8598',
   (p5: P5, analyzer: Tone.Analyser) => {
     const width = window.innerWidth;
     const height = window.innerHeight / 2;
-    let averageVal = 0;
 
     p5.background(0, 0, 0, 255);
 
@@ -18,33 +17,24 @@ export const CircularVisualizer = new Visualizer(
 
     let values = analyzer.getValue();
 
-    if (values.length > 40) {
-      values = values.slice(0, 40);
+    if (values.length > 360) {
+      values = values.slice(0, 360);
     }
 
-    for (let i = 0; i < values.length; i++) {
-      averageVal += (values[i] as number * 2) ** 2;
-    }
+    const idx = 100;
+    const val = values[idx] as number * 10000;
 
-    averageVal = Math.min(1000 * averageVal, 255);
-    const shade = (averageVal / (values[0] as number));
-    p5.fill(shade, 0, averageVal, 255);
-    p5.strokeWeight(2);
-
-    console.log(values);
+    p5.stroke(val, 100, val, 255);
+    p5.strokeWeight(3);
+    p5.translate(width / 2, height / 2);
     p5.beginShape();
     for (let i = 0; i < values.length; i++) {
       let amplitude = values[i] as number
-      let ampMap = Math.min(1000 * amplitude, 255)
-      if (ampMap < 0) {
-        ampMap = -1 * ampMap;
-      }
+      let r = p5.map(amplitude, 0, 1, 30, 2 * height);
+      let x = r * Math.cos(i);
+      let y = r * Math.sin(i);
 
-      let r = p5.map(amplitude, 0, 1, 0, height);
-      let x = r * Math.cos(i) + (1.05 * Math.floor(width / 2)) - Math.floor(width / 10);
-      let y = r * Math.sin(i) + Math.floor(height / 2);
-
-      p5.rect(x, y, r, r);
+      p5.point(x, y);
     }
     p5.endShape();
   },
